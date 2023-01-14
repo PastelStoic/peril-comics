87dafbc562bda13a87fc257bcfad450ab7c60f59
PastelStoic <pastelstoic@proton.me>
2023-01-13 21:37:24 -0500

    Removed old

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,6 @@ const config = {
     defaultLocale: "en",
   },
   experimental: {
-    appDir: true,
   }
 };
 export default config;
--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Peril Comics</title>
-      <meta name="description" content="The ultimate destination for peril comics." />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  );
-}
