04b427b82d270ba1e9a5b37eeb021ac76d031370
PastelStoic <pastelstoic@proton.me>
2023-07-01 13:01:22 -0400

    Fixed mistaken auth

--- src/server/trpc/router/comic.ts
+++ src/server/trpc/router/comic.ts
@@ -30,25 +30,28 @@ export const comicRouter = router({
   .query(async ({ctx, input}) => {
     const result = await getComicByTitle(ctx.edgedb, input);
     // if comic is not public and there is no authed user, tell them to sign in
-    if (!result?.is_free || !ctx.session?.user) {
-      throw new TRPCError({ 
-        code: "UNAUTHORIZED",
-        message: `You must be a supporter at $15.00 or above to view this content, or be logged in with a guest access code.`
-      });
-    } 
-    // if they're not an admin or guest, check if they're a supporter
-    if (ctx.session.user.role !== "admin" && ctx.session.user.role !== "guest") {
-      const supporterData = await Promise.all([
-        checkPatreonStatus(ctx.session.user.id, ctx.edgedb), 
-        checkSubscribestarStatus(ctx.session.user.id, ctx.edgedb)
-      ]);
-      if (!supporterData.some(data => data.supportAmount >= supporterPaymentMin)) {
+    if (!result?.is_free) {
+      if (!ctx.session?.user) {
         throw new TRPCError({ 
           code: "UNAUTHORIZED",
-          message: `You must be a supporter at $15.00 or above to view this content. Your current support level is $${Math.round(Math.max(...supporterData.map(d => d.supportAmount)) / 100)}.00. Your role is ${ctx.session.user.role}`
+          message: `You must be a supporter at $15.00 or above to view this content, or be logged in with a guest access code.`
         });
+      } else {
+        // if they're not an admin or guest, check if they're a supporter
+        if (ctx.session.user.role !== "admin" && ctx.session.user.role !== "guest") {
+          const supporterData = await Promise.all([
+            checkPatreonStatus(ctx.session.user.id, ctx.edgedb), 
+            checkSubscribestarStatus(ctx.session.user.id, ctx.edgedb)
+          ]);
+          if (!supporterData.some(data => data.supportAmount >= supporterPaymentMin)) {
+            throw new TRPCError({ 
+              code: "UNAUTHORIZED",
+              message: `You must be a supporter at $15.00 or above to view this content. Your current support level is $${Math.round(Math.max(...supporterData.map(d => d.supportAmount)) / 100)}.00. Your role is ${ctx.session.user.role}`
+            });
+          }
+        }
       }
-    }
+    } 
 
     if (result) return result;
     throw new TRPCError({message: `No comic "${input.title}" could be found.`, code: "NOT_FOUND"});
