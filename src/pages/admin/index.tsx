c26535e50af932a2c113d4def5c12ca10a28a9bf
PastelStoic <pastelstoic@proton.me>
2023-07-02 19:17:58 -0400

    Removed dead link

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,8 +4,6 @@ export default function AdminHome() {
   return (
     <>
     <Link href={"/admin/comics"}>Edit Comics</Link>
-    <br />
-    <Link href={"/admin/images"}>Upload or Manage Images</Link>
     </>
   );
 }
